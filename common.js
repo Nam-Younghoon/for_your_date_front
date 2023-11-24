a55edb3fdd3e2a65ade9e52cbd8d928dcc086b09
eric <huny3410@gmail.com>
2023-11-24 14:43:15 +0900

    add: common.js에 baseURL 추가

--- common.js
+++ common.js
@@ -1,3 +1,5 @@
+const baseURL = "http://127.0.0.1:8000/"
+
 function dateParser(dateStr) {
     const date = new Date(dateStr)
     return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
